2bd2f4d76ca7b31b2770f723b8d229dd1141ed7b
cara <wq_hnu@163.com>
2016-09-26 16:29:02 +0800

    Create module demo

---
+++ school/group.js
@@ -0,0 +1,15 @@
+var student = require('./student')
+var teacher = require('./teacher')
+
+teacher.add('Cara')
+// student.add('kyo')
+function add (teacherName, students) {
+  teacher.add(teacherName)
+  students.forEach(function (item, index) {
+    student.add(item)
+  })
+// student.add('students')
+}
+
+exports.add = add
+module.exports = add
---
+++ school/index.js
@@ -0,0 +1,10 @@
+var group = require('./group')
+
+exports.add = function (groups) {
+  groups.forEach(function (item, index) {
+    var group = item
+    var teacherName = item.teacherName
+    var students = item.students
+  })
+  group.add('teacherName', ['kyo', 'kyo'])
+}
